7f247f9d18c308e9e286e2bf83f9103e18857cae
Ayush Majumdar <64153762+Ayush6501@users.noreply.github.com>
2021-02-04 19:36:25 +0530

    Create Grabber.h

---
+++ DungeonEscape/Grabber.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DUNGEONESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	float Reach = 100.f;
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
+
+	void Grab();
+	void Release();
+	void CheckPhysicsHandle();
+	void SetupInputComponent();
+
+	//Returns First Actor Within Reach 
+	FHitResult GetFirstPhysicsBodyInReach() const;
+
+	//Get Player's reach i.e LineTraceEnd
+	FVector GetLineTraceEnd() const;
+
+	//Get Player's Position in the World
+	FVector GetPlayersWorldPos() const;		
+};
